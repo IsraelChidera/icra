38364ea2d154c8a5fc089a889662ea1bb2f675b3
IsraelChidera <israelchidera54@gmail.com>
2023-08-01 10:50:32 +0100

    Project setup: Icra  landing page

---
+++ components/Navbar.jsx
@@ -0,0 +1,9 @@
+'use client'
+
+const Navbar = () => {
+  return (
+    <nav>Navbar</nav>
+  )
+}
+
+export default Navbar
---
+++ constants/index.js
@@ -0,0 +1,14 @@
+export const socials = [
+  {
+    name: "twitter",
+    url: "/images/twitter.svg",
+  },  
+  {
+    name: "instagram",
+    url: "/images/instagram.svg",
+  },
+  {
+    name: "facebook",
+    url: "/images/facebook.svg",
+  },
+];
